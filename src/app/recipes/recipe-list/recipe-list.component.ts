b2f8b8da78536168b5d2e554a6eb94d7f3e05406
Dylan Giles <dylanygiles@gmail.com>
2024-03-25 15:40:02 +1100

    lesson 63

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,13 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
+const TEMPLATE_IMAGE =
+  'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css',
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [];
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', TEMPLATE_IMAGE),
+
+    new Recipe(
+      'Another test recipe',
+      'This is simply another test recipe',
+      TEMPLATE_IMAGE
+    ),
+  ];
 
   ngOnInit() {}
 }
